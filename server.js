d4a82215b39f4375fa852fc29efe5dfd45d8056d
arthursiq5 <arthur.siq5@gmail.com>
2024-09-17 18:17:25 -0300

    Adicionadas rotas

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
 const app = express();
 
 // Porta onde o servidor vai rodar
@@ -7,6 +8,9 @@ const PORT = 8080;
 
 // Conexão com banco de dados
 const db = new sqlite3.Database('./database.db');
+
+// permitindo receber dados via json
+app.use(bodyParser.json());
 
 // Rota que redireciona
 app.get('/rotas/:rota', (req, res) => {
@@ -28,6 +32,41 @@ app.get('/rotas/:rota', (req, res) => {
 
         // Redireciona para a URL encontrada
         res.redirect(row.url);
+    });
+});
+
+app.post('/rotas', (req, res) => {
+    console.log(req.body)
+    const { rota, nome, url } = req.body;
+
+    if (!rota || !url) {
+        return res.status(400).send('Campos obrigatórios: rota e url');
+    }
+
+    // Inserir os dados na tabela "links"
+    const query = 'INSERT INTO links (rota, nome, url) VALUES (?, ?, ?)';
+    db.run(query, [rota, nome, url], function (err) {
+        if (err) {
+            console.error('Erro ao inserir no banco de dados:', err.message);
+            res.status(500).send('Erro ao inserir no banco de dados');
+        } else {
+            res.send(`Rota adicionada com sucesso! ID: ${this.lastID}`);
+        }
+    });
+});
+
+// listando todas as rotas
+app.get('/rotas', (req, res) => {
+    // Consulta todas as rotas no banco de dados
+    db.all('SELECT * FROM links', (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Erro no servidor');
+            return;
+        }
+
+        // Retorna todas as rotas como JSON
+        res.json(rows);
     });
 });
 
